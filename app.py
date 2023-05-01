0e445aee659c12f61804263325e2a8213f0d57d8
Jpduker <jayaprakashnis619@gmail.com>
2023-05-01 22:03:00 +0530

    added CRUD restful API code

--- app.py
+++ app.py
@@ -1,77 +1,46 @@
-from flask import Flask, jsonify, request
-from flask_pymongo import PyMongo
-from bson import ObjectId
+from flask import Flask
+from flask_restful import Api
 import os
-from dotenv import load_dotenv
-load_dotenv()
+from bson.objectid import ObjectId
+from flask import request
+from flask_restful import Resource
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = os.getenv('MONGO_URI')
-app.config["MONGO_DBNAME"] = "mydatabase"
-mongo = PyMongo(app)
-db = mongo.db
+api = Api(app)
 
-#route for home page and return a json response
-@app.route('/',methods=['GET'])
-def home():
-    return jsonify({'message': 'Welcome to my API'})
+from pymongo import MongoClient
+
+# Replace the connection string with your own
+client = MongoClient(os.getenv('MONGO_URI'))
+
+# Replace "mydatabase" with the name of your database
+db = client.mydatabase
 
 
-# GET all users
-@app.route('/users', methods=['GET'])
-def get_all_users():
-    users = db.users.find()
-    output = []
-    for user in users:
-        output.append({'id': str(user['_id']), 'name': user['name'], 'email': user['email'], 'password': user['password']})
-    return jsonify({'result': output})
+class Create(Resource):
+    def post(self):
+        data = request.get_json()
+        result = db.mycollection.insert_one(data)
+        return {'id': str(result.inserted_id)}
 
-# GET user by id
-@app.route('/users/<id>', methods=['GET'])
-def get_user_by_id(id):
-    user = db.users.find_one({'_id': ObjectId(id)})
-    if user:
-        output = {'id': str(user['_id']), 'name': user['name'], 'email': user['email'], 'password': user['password']}
-    else:
-        output = 'User not found'
-    return jsonify({'result': output})
-#test
-@app.route('/test-db', methods=['GET'])
-def test_db():
-    try:
-        db.collection_names()
-        return jsonify({'message': 'Successfully connected to MongoDB'})
-    except Exception as e:
-        return jsonify({'message': 'Could not connect to MongoDB', 'error': str(e)})
+class Read(Resource):
+    def get(self, id):
+        result = db.mycollection.find_one({'_id': ObjectId(id)})
+        if result:
+            result['_id'] = str(result['_id'])
+            return result
+        else:
+            return {'error': 'Not found'}, 404
 
-# POST a new user
-@app.route('/users', methods=['POST'])
-def add_user():
-    user = {'name': request.json['name'], 'email': request.json['email'], 'password': request.json['password']}
-    db.users.insert_one(user)
-    return jsonify({'result': 'User added successfully'})
-
-# PUT (update) user by id
-@app.route('/users/<id>', methods=['PUT'])
-def update_user(id):
-    user = db.users.find_one({'_id': ObjectId(id)})
-    if user:
-        db.users.update_one({'_id': ObjectId(id)}, {'$set': {'name': request.json['name'], 'email': request.json['email'], 'password': request.json['password']}})
-        output = 'User updated successfully'
-    else:
-        output = 'User not found'
-    return jsonify({'result': output})
-
-# DELETE user by id
-@app.route('/users/<id>', methods=['DELETE'])
-def delete_user(id):
-    user = db.users.find_one({'_id': ObjectId(id)})
-    if user:
-        db.users.delete_one({'_id': ObjectId(id)})
-        output = 'User deleted successfully'
-    else:
-        output = 'User not found'
-    return jsonify({'result': output})
-
+class Update(Resource):
+    def put(self, id):
+        data = request.get_json()
+        result = db.mycollection.update_one({'_id': ObjectId(id)}, {'$set': data})
+        if result.modified_count == 1:
+            return {'message': 'Updated successfully'}
+        else:
+            return {'error': 'Not found'}, 404``
+        
+        
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ app_non_rest.py
@@ -0,0 +1,77 @@
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo
+from bson import ObjectId
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+app.config["MONGO_DBNAME"] = "mydatabase"
+mongo = PyMongo(app)
+db = mongo.db
+
+#route for home page and return a json response
+@app.route('/',methods=['GET'])
+def home():
+    return jsonify({'message': 'Welcome to my API'})
+
+
+# GET all users
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    users = db.users.find()
+    output = []
+    for user in users:
+        output.append({'id': str(user['_id']), 'name': user['name'], 'email': user['email'], 'password': user['password']})
+    return jsonify({'result': output})
+
+# GET user by id
+@app.route('/users/<id>', methods=['GET'])
+def get_user_by_id(id):
+    user = db.users.find_one({'_id': ObjectId(id)})
+    if user:
+        output = {'id': str(user['_id']), 'name': user['name'], 'email': user['email'], 'password': user['password']}
+    else:
+        output = 'User not found'
+    return jsonify({'result': output})
+#test
+@app.route('/test-db', methods=['GET'])
+def test_db():
+    try:
+        db.collection_names()
+        return jsonify({'message': 'Successfully connected to MongoDB'})
+    except Exception as e:
+        return jsonify({'message': 'Could not connect to MongoDB', 'error': str(e)})
+
+# POST a new user
+@app.route('/users', methods=['POST'])
+def add_user():
+    user = {'name': request.json['name'], 'email': request.json['email'], 'password': request.json['password']}
+    db.users.insert_one(user)
+    return jsonify({'result': 'User added successfully'})
+
+# PUT (update) user by id
+@app.route('/users/<id>', methods=['PUT'])
+def update_user(id):
+    user = db.users.find_one({'_id': ObjectId(id)})
+    if user:
+        db.users.update_one({'_id': ObjectId(id)}, {'$set': {'name': request.json['name'], 'email': request.json['email'], 'password': request.json['password']}})
+        output = 'User updated successfully'
+    else:
+        output = 'User not found'
+    return jsonify({'result': output})
+
+# DELETE user by id
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    user = db.users.find_one({'_id': ObjectId(id)})
+    if user:
+        db.users.delete_one({'_id': ObjectId(id)})
+        output = 'User deleted successfully'
+    else:
+        output = 'User not found'
+    return jsonify({'result': output})
+
+if __name__ == '__main__':
+    app.run(debug=True)
